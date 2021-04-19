b54f1b8acbf0650860dfa45caded985beeef0e3f
mxdi9i7 <peter.zheng88228@gmail.com>
2021-04-19 01:28:40 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import Column from './Column';
+import React from 'react';
+
+const initialState = [
+  {
+    title: 'Col 1',
+    todos: ['hello', 'foo'],
+    color: 'yellow',
+    input: '',
+  },
+  {
+    title: 'Col 2',
+    todos: ['hello', 'foo'],
+    color: 'red',
+    input: '',
+  },
+  {
+    title: 'Col 3',
+    todos: ['hello', 'foo'],
+    color: 'green',
+    input: '',
+  },
+  {
+    title: 'Col 4',
+    todos: ['hello', 'foo'],
+    color: 'purple',
+    input: '',
+  },
+];
 
 function App() {
+  const [col, setCol] = React.useState(initialState);
+
+  const handleInputChange = (value, colIndex) => {
+    const nextCol = [...col];
+    nextCol[colIndex].input = value;
+    setCol(nextCol);
+  };
+
+  const handleAddTodo = (colIndex) => {
+    const nextCol = [...col];
+    if (nextCol[colIndex].input) {
+      nextCol[colIndex].todos.push(nextCol[colIndex].input);
+      nextCol[colIndex].input = '';
+      setCol(nextCol);
+    }
+  };
+
+  const handleShift = (itemIndex, fromCol, toCol) => {
+    const nextCol = [...col];
+    const temp = nextCol[fromCol].todos[itemIndex];
+    nextCol[fromCol].todos.splice(itemIndex, 1);
+    nextCol[toCol].todos.push(temp);
+    setCol(nextCol);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {col.map((v, i) => (
+        <Column
+          index={i}
+          handleInputChange={handleInputChange}
+          data={v}
+          key={i}
+          handleAddTodo={handleAddTodo}
+          handleShift={handleShift}
+          isFirst={i === 0}
+          isLast={i === col.length - 1}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/Column.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Item from './Item';
+
+export default function Column({
+  data,
+  handleInputChange,
+  index,
+  handleAddTodo,
+  handleShift,
+  isFirst,
+  isLast,
+}) {
+  return (
+    <div className='col'>
+      <h3 style={{ backgroundColor: data.color }}>{data.title}</h3>
+      {data.todos.map((v, i) => (
+        <Item
+          key={i}
+          itemTitle={v}
+          isFirst={isFirst}
+          isLast={isLast}
+          handleShift={handleShift}
+          index={i}
+          colIndex={index}
+        />
+      ))}
+      <input
+        onChange={(e) => handleInputChange(e.target.value, index)}
+        value={data.input}
+        placeholder='enter some value'
+      />
+      <button onClick={() => handleAddTodo(index)}>Add todo</button>
+    </div>
+  );
+}
